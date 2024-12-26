eb81926af29267bb6c4f622fbe46b20252903615
SALLA ABHINAYA <141430666+SALLA-ABHINAYA@users.noreply.github.com>
2024-12-26 21:50:00 +0530

    Add files via upload

---
+++ Day-24.sql
@@ -0,0 +1,16 @@
+-- Question: Santa is tracking how many presents he delivers each night leading up to Christmas. He wants a running total to see how many gifts have been delivered so far on any given night. Using the deliveries table, calculate the cumulative sum of gifts delivered, ordered by the delivery date.
+-- Table name: deliveries
+CREATE TABLE deliveries (
+    delivery_date DATE PRIMARY KEY,
+    gifts_delivered INT
+);
+INSERT INTO deliveries (delivery_date, gifts_delivered) VALUES
+('2024-12-20', 120),
+('2024-12-21', 150),
+('2024-12-22', 200),
+('2024-12-23', 300),
+('2024-12-24', 500);
+
+-- Query
+select delivery_date,sum(gifts_delivered)over(order by delivery_date) as cumulative_sum
+from deliveries
